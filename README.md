9cf599fd0c81d1276f74a8fe106e3bf8a4cb48c3
Kriss Rauhvargers <Kriss.Rauhvargers@lu.lv>
2024-04-18 23:49:55 +0300

    Added basic models and their migrations

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 
-### Setting up this demo
+## Setting up this demo
 
-Commands to be executed
+# To start from scratch
+Commands to be executed to create this project from scratch
 
+```bat
     mkdir lab3lara
     cd lab3lara
 
@@ -18,7 +20,17 @@ Commands to be executed
 
     ddev php artisan migrate
     ddev artisan make:controller AutomobileController
-
+```
 
 A good fix for "php executable path not set" problem by some development tools
 https://stackoverflow.com/a/73630729/239599
+
+```bat
+php artisan make:model Color --migration 
+php artisan make:model Fuel --migration 
+php artisan make:model Country --migration 
+php artisan make:model Manufacturer --migration 
+php artisan make:model Car --migration 
+```
+
+# To run it on your computer
---
+++ database/migrations/2024_04_18_195834_create_cars_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('carmodel_id')->constrained()->onDelete('cascade');
+            $table->foreignId('color_id')->constrained()->onDelete('cascade');
+            $table->foreignId('fuel_id')->constrained()->onDelete('cascade');
+            $table->integer('registration_year');
+            $table->foreignId('country_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cars');
+    }
+};
