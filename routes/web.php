891f7837e8aa251a0dba000c6a7cdc570e15b477
Kriss Rauhvargers <Kriss.Rauhvargers@gmail.com>
2024-05-09 22:29:56 +0300

    Added breeze and fixed vite.js for DDEV

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AutomobileController;
-use App\Http\Controllers\CarmodelController;
 
 Route::get('/', function () {
     return view('welcome');
-})->name('home');
+});
 
-Route::get('/automobiles', [AutomobileController::class, 'index'])->name('automobiles.index');
-Route::get('/models/{carmodel}', [CarmodelController::class, 'show'])->name('models.show');
-Route::get('/models/{carmodel}/edit', [CarmodelController::class, 'edit'])->name('models.edit');
-Route::put('/models/{carmodel}/update', [CarmodelController::class, 'update'])->name('models.update');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
